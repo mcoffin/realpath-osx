e94f5a918ece96d6f509923c84cdb15f4859d353
Matt Coffin <mcoffin@irys.com>
2012-12-01 11:43:15 -0700

    Add platform checks to cmake and remove Makefile

--- Makefile
+++
@@ -1,6 +0,0 @@
-BIN=realpath
-compile:
-	gcc -Wall -o ${BIN} ${BIN}.c
-install:
-	cp -f ${BIN} /usr/bin/${BIN}
-
--- realpath.c
+++ realpath.c
@@ -4,7 +4,6 @@
  */
 
 #include <sys/param.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char** argv) {
